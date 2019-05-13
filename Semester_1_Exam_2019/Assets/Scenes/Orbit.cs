2e0956aec1731923452ab34ae4c41bcf694aded9
APCTophat <49924900+APCTophat@users.noreply.github.com>
2019-05-13 18:32:55 +0200

    messing with Orbit

---
+++ Semester_1_Exam_2019/Assets/Scenes/Orbit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject Center;
+    public GameObject Player;
+
+    public Rigidbody rb;
+
+    public int X_axis;
+    public int Y_axis;
+
+    public float OrbitSpeed;
+    
+
+
+
+    void Start()
+    {
+        //transform.LookAt(Center.transform);
+        rb = Player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(Center.transform);
+        //float HDirect = Input.GetAxis("Horizontal");
+        //rb.velocity = new Vector3(HDirect * 10, rb.velocity.y, rb.velocity.z);
+
+        transform.RotateAround(Center.transform.position, new Vector3(X_axis,Y_axis,0), OrbitSpeed * Time.deltaTime);
+     }  
+}
